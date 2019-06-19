75a1b2ba1daaea9aec0234060d73ecb61f55bb8a
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-06-19 02:14:42 -0400

    added about page and members schema

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const   express     = require("express"),
         router      = express.Router(),
-        passport    =require("passport");
+        passport    =require("passport"),
+        Member      =require("../models/member");
 
 router.get("/",(req,res)=>{
     res.render("landing");
 });
 
+router.get("/about",(req,res)=>{
+    Member.find({},(err,allMember)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("about",{ members:allMember});
+        }
+    });
+});
 module.exports = router;
