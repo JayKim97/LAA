ec18211949bb226f6ca4f268401c62548532d7cf
Jikyung Colin Kim <colinjkim1997@gmail.com>
2019-09-24 16:15:32 -0400

    finalized members page and got front page slideshow working

--- v3/models/sponsor.js
+++ v3/models/sponsor.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const SponsorSchema = new mongoose.Schema({
     title: String,
-    image: String
+    image: String,
+    tier: String
 });
 
 module.exports = mongoose.model("Sponsor", SponsorSchema);
